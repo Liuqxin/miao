15946b225b2b0b7632a8ecc0a8903cf8b1611c96
Liuqxin <1359814884@qq.com>
2022-09-14 22:11:34 +0800

    ''

--- lodash/liuqxin-lodash.js
+++ lodash/liuqxin-lodash.js
@@ -1,4 +1,4 @@
-var lodash = {
+var liuqxin = {
 
   chunk: function (array, size) {
     var res = {}
@@ -38,11 +38,87 @@ var lodash = {
     return array
   },
   drop: function (array, n) {
-    for (var i = 0; i < array.length; i++){
-      array[i]=value
+    var res = []
+    if (n > array.length) {
+      return []
     }
-    return array
+    for (var i = 0; i < n; i++){
+      res[i]=array[i]
+    }
+    return res
   },
+  findIndex: function (array, x) {
+    for (var i = 0; i < array.length; i++){
+      if (res[i] == x) {
+        return i
+      }
+    }
+    return -1
+  },
+  findLastIndex: function (array, x) {
+    for (var i = array.length-1; i >=0; i--){
+      if (res[i] == x) {
+        return i
+      }
+    }
+    return -1
+  },
+  flatten: function (array) {
+    var res = []
+    for (var i = 0; i < array.length; i++){
+      for (var j = 0; j < array[i].length; j++){
+        res.push(array[i][j])
+      }
+    }
+    return res
+  },
+  flattenDeep: function (array) {
+    if (flatten(array) == this.flattenDeep(array)) {
+      return array
+    }
+    flatten(array)
+  },
+
+  flattenDepth: function (array,n) {
+    var res = []
+    while (n) {
+      array = res
+      for (var i = 0; i < array.length; i++){
+        for (var j = 0; j < array[i].length; j++){
+          res.push(array[i][j])
+        }
+      }
+      n--
+    }
+    return res
+  },
+  fromPairs: function (array) {
+    var map = {}
+    for (var i = 0; i < array.length;i++) {
+      map[array[i][0]]=array[i][1]
+    }
+    return map
+
+  },
+  toPairs: function (object) {
+    var map = []
+    for (var key of object) {
+      var res = []
+      res[0] = key
+      res[i] = object[key]
+      map.push(res)
+    }
+    return map
+
+  },
+  head: function (array) {
+    if (array.length == 0) {
+      return array
+    }
+    return array[0]
+
+  },
+
 
 
 
